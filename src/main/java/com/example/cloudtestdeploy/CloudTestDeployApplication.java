bf05f1868486880c37d0b26b9e9cc1d9f0ae38d6
patwork365 <patwork365@gmail.com>
2022-12-05 09:49:37 -0500

    springboot deploy in cloud

---
+++ src/main/java/com/example/cloudtestdeploy/CloudTestDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudtestdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTestDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTestDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudtestdeploy/CloudTestDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudtestdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTestDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
